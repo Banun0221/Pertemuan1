19faecf652ea3ed0bf274a8b819118f5c231a717
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-03-11 16:50:51 +0700

    3_Komentar

--- src/main/kotlin/3_Komentar.kt
+++ src/main/kotlin/3_Komentar.kt
@@ -1,3 +1,12 @@
+import kotlin.jvm.internal.Intrinsics.Kotlin
+
 fun main() {
-    //
+    println("Hello, ini Program Pertama Saya di Kotlin!")
+    //Contoh komenter satu baris
+    /*
+    Contoh Komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+      */
 }
