e4f7814d7614cdcf67a8afc908058a690451216e
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-04-08 10:12:47 +0700

    16_String_Array_of_Character.kt

--- src/main/kotlin/16_String_Array_of_Character.kt
+++ src/main/kotlin/16_String_Array_of_Character.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-val myName: String = "Depandi Enda"
+val myName: String = "Syahbana"
 for(chr in myName){
     print(chr)
-}
-print('\n') {
-val s = "abc" + 1
-println(s + "def")
-}
-
 }
